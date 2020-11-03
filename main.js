de5153b501c4a0e597714ba57a7b59badd593a50
Erinle Feyijimi <erinlejhimmy@gmail.com>
2020-11-03 21:27:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var icon = document.getElementById("nav-icon");
-var nav = document.getElementById("nav");
+'use strict';
+
+const rating = document.querySelector(".ratings");
+
+let icon = document.getElementById("nav-icon");
+let nav = document.getElementById("nav");
 icon.addEventListener("click", function(){
     nav.classList.toggle("open")
 })
 
-// Function to change webpage background color
-    function changeBodyBg(color){
-        document.body.style.background = color;
+
+let changeBG = false;
+
+function changeBackgroundColor(color){
+    document.body.style.background = color;
+    if (!changeBG){
+        if (color !== '#0392BF'){
+            document.body.style.color = "rgba(0,0,0,0.7)";
+            size.classList.add("dark");
+        } changeBG = true;
+    } else if(color === '#0392BF'){
+        document.body.style.color = "#f7e2e2";
+        size.classList.remove("dark");
+        changeBG = false;
     }
-    
+} 
