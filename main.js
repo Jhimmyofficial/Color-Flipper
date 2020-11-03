e5a68e16cb833bb1f80a88f46c25c0d9c355120e
Erinle Feyijimi <erinlejhimmy@gmail.com>
2020-11-03 16:49:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ var nav = document.getElementById("nav");
 icon.addEventListener("click", function(){
     nav.classList.toggle("open")
 })
+
+// Function to change webpage background color
+    function changeBodyBg(color){
+        document.body.style.background = color;
+    }
+    
